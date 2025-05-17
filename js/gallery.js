11b982d4dc1a9b91dd7c7ab0403a7fd0c0afdb33
chivasior042 <042@chivasior.pl>
2025-05-17 05:24:06 +0200

    Update gallery.js

--- js/gallery.js
+++ js/gallery.js
@@ -1,11 +1,17 @@
 // Pobieramy listę plików z images/images.json
 fetch('images/images.json')
-  .then(res => res.json())
+  .then(res => {
+    if (!res.ok) {
+      throw new Error(`HTTP error! status: ${res.status}`);
+    }
+    return res.json();
+  })
   .then(list => {
     const container = document.getElementById('gallery-container');
     list.forEach((fname, i) => {
       const img = document.createElement('img');
       img.src = `images/${fname}`;
+      img.alt = `Obraz ${i + 1}`;
       // opóźnienie animacji kolejnych zdjęć
       img.style.animationDelay = `${i * 0.1}s`;
       container.appendChild(img);
